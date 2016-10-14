568fe3190189f317c02dd1ecb4c0ee92fd79f5b9
roycetanjiashing <roycetan@roycetahingsmbp.gateway.2wire.net>
2016-10-14 20:09:33 +0800

    Initial Commit

---
+++ Breakout/GameScene.swift
@@ -0,0 +1,79 @@
+//
+//  GameScene.swift
+//  Breakout
+//
+//  Created by roycetanjiashing on 14/10/16.
+//  Copyright © 2016 examplecompany. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene,SKPhysicsContactDelegate {
+    var ball:SKSpriteNode!
+    var paddle:SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "Ball") as! SKSpriteNode
+        paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
+        
+        self.physicsWorld.contactDelegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
+        }
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyAName = contact.bodyA.node?.name
+        let bodyBName = contact.bodyB.node?.name
+        
+        if bodyAName == "Ball" && bodyBName == "Brick" || bodyAName == "Brick" && bodyBName == "Ball"{
+            if bodyAName == "Brick" {
+                contact.bodyA.node?.removeFromParent()
+            } else if bodyBName == "Brick" {
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
